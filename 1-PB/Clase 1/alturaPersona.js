ac75732e1ff633a32268d311c7fb7554eab6bdff
Docente CFL PFS <104649327+Docente-CFL-PFS@users.noreply.github.com>
2022-05-18 12:58:59 -0300

    Delete alturaPersona.js

--- 1-PB/Clase 1/alturaPersona.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var readlineSync = require("readline-sync");
-var alturaPersona = readlineSync.question("Indique la altura de la persona: ");
-console.log("La altura de la persona es: ", alturaPersona);
